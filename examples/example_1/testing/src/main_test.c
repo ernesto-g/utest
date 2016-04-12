539ed7836d4cb6cf1b256396822930a1119e9576
Mauro <mauro@mauro>
2016-04-12 15:59:12 -0300

    Proyecto ordenado

--- examples/example_1/testing/src/main_test.c
+++ examples/example_1/testing/src/main_test.c
@@ -1,6 +1,6 @@
 /*
-    utest example : Unit test library for embedded C programs.
-    Copyright (C) <2016>  <Ernesto Gigliotti>
+    utest example : Unit test examples.
+    Copyright (C) <2016>  <Mauricio Davila>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -15,6 +15,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+
 #include <utest.h>
 #include <stdio.h>
 #include <stdlib.h>
